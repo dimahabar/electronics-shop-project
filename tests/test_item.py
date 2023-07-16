45f0f4c206ae5b64648b434be4e42a3377b32372
Dmitriy Kovaluk <kdi.86@mail.ru>
2023-07-16 14:41:48 +1000

    test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
+import pytest
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+def test_item_all():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert len(Item.all) == 2
+
+def test_calculate_total_price():
+    assert 100 * 20 == 2000
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    assert 100 * Item.pay_rate == 50
